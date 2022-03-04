dbec155f390f90c9a2d6c994c87390a165c84c58
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2022-03-04 11:52:23 +0700

    cek

--- PAPSI_SI_Bakery/PAPSI_SI_Bakery/Form1.Designer.vb
+++ PAPSI_SI_Bakery/PAPSI_SI_Bakery/Form1.Designer.vb
@@ -32,7 +32,7 @@ Partial Class Form1
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(428, 31)
         Me.TextBox1.TabIndex = 0
-        Me.TextBox1.Text = "BAKERY"
+        Me.TextBox1.Text = "SI BAKERY"
         Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Form1
